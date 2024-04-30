e54027df82f4066dc3c6535e6a417df2f4e61529
Sharad Joshi <sharad.joshi@natwest.com>
2024-04-30 18:03:50 +0100

    count all characters using array

--- chapter-1/explicit_special.c
+++ chapter-1/explicit_special.c
@@ -6,14 +6,21 @@ int main()
     int c;
     int count;
 
-    c = getchar();
-    while (c!=EOF) {
-        if (c=='\t')
-            putchar('\t');
-        else if (c=='\b')
+    while ((c=getchar())!=EOF) {
+        if (c=='\t'){
             putchar('\\');
+            putchar('t');
+        }
+        else if (c=='\b'){
+            /* this never works due to backspace properties - i need to understand this better*/
+            putchar('\\');
+            putchar('b')''
+        }
+        else if (c=='\\'){
+            putchar('\\');
+            putchar('\\');
+        }
         else 
             putchar(c);
-        c = getchar();
     }
 }
